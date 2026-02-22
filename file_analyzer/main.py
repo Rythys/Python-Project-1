6364ec118c8a49d1123241e74311711ba2311610
Bob <r_yu_00@bk.ru>
2026-02-23 00:43:35 +0300

    comleted filters.py, scanner.py. Added some funtion to utils.py and to main.py added top files list and statistics by extensions

--- file_analyzer/main.py
+++ file_analyzer/main.py
@@ -1,17 +1,32 @@
 import argparse
+from scanner import scanner
+from utils import statistic_by_extensions
+
 
 def main():
+
     parser = argparse.ArgumentParser()
-    parser.add_argument("PATH")
-    # parser.add_argument("-h", "--help")
-    parser.add_argument("--ext")
-    parser.add_argument("--min-size")
-    parser.add_argument("--max-size")
-    parser.add_argument("--name")
-    parser.add_argument("--top")
-    parser.add_argument("--output")
-    parser.add_argument("--json")
+    
+    parser.add_argument("path", type=str)
+    parser.add_argument("--ext", type=str, default=None)
+    parser.add_argument("--min-size", type=int, default=0)
+    parser.add_argument("--max-size", type=int, default=None)
+    parser.add_argument("--name", type=str, default=None)
+    parser.add_argument("--top", type=int, default=5)
+    parser.add_argument("--output", type=str, default=None)
+    parser.add_argument("--json", type=bool, default=False)
+    
     args = parser.parse_args()
-    print(args.PATH, args.top)
+    all_filtered_paths_list = list(scanner(args))
+
+    if args.top:
+        top_filtered_paths_list = sorted(all_filtered_paths_list, \
+                                         key=lambda x: x["size"], \
+                                         reverse=True)[:args.top]
+    # print(top_filtered_paths_list)
+
+    extensions_stats_dict = statistic_by_extensions(all_filtered_paths_list)
+    # print(extensions_stats_dict)
+    
 if __name__ == "__main__":
     main()
